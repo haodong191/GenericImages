3a152c67936af035fd3075a8a199649b95d842e2
Hao Dong <hdong215@gmail.com>
2019-07-18 16:58:33 -0400

    Update index.js

--- index.js
+++ index.js
@@ -1,14 +1,65 @@
+var Botkit = require('botkit');
+
+if (!process.env.12010145056.692826213313 || !process.env.bde09e9c4dabb1975854c7038e7fe446 || !process.env.3000 || !process.env.wIxaLrOv2ldVyszsGnrVf3Dk) {
+    console.log('Error: Specify CLIENT_ID, CLIENT_SECRET, VERIFICATION_TOKEN and PORT in environment');
+    process.exit(1);
+}
+
+var config = {}
+if (process.env.MONGOLAB_URI) {
+    var BotkitStorage = require('botkit-storage-mongo');
+    config = {
+        storage: BotkitStorage({mongoUri: process.env.MONGOLAB_URI}),
+    };
+} else {
+    config = {
+        json_file_store: './db_slackbutton_slash_command/',
+    };
+}
+
+var controller = Botkit.slackbot(config).configureSlackApp(
+    {
+        clientId: process.env.CLIENT_ID,
+        clientSecret: process.env.CLIENT_SECRET,
+        scopes: ['commands'],
+    }
+);
+
+controller.setupWebserver(process.env.PORT, function (err, webserver) {
+    controller.createWebhookEndpoints(controller.webserver);
+
+    controller.createOauthEndpoints(controller.webserver, function (err, req, res) {
+        if (err) {
+            res.status(500).send('ERROR: ' + err);
+        } else {
+            res.send('Success!');
+        }
+    });
+});
+
 controller.on('slash_command', function (slashCommand, message) {
 
     switch (message.command) {
-        case "/gimages":
-            if (message.token !== process.env.wIxaLrOv2ldVyszsGnrVf3Dk) return;
-            if (message.text === "") {
+        case "/echo":
+            if (message.token !== process.env.wIxaLrOv2ldVyszsGnrVf3Dk) return; 
+            if (message.text === "" || message.text === "help") {
                 slashCommand.replyPrivate(message,
-                    "https://www.revzilla.com/assets/0002/7492/Generic_Hardware_for_PDP.jpeg https://www.revzilla.com/assets/0002/7493/Generic_Sunshield_for_PDP.jpeg https://www.revzilla.com/assets/0002/7494/Generic_Face_Shield_for_PDP.jpeg https://www.revzilla.com/assets/0002/7495/Generic_Cheek_Pads_for_PDP.jpeg https://www.revzilla.com/assets/0002/7496/Generic_Helmet_Liner_for_PDP.jpeg https://www.revzilla.com/assets/0002/7497/Generic_Back_Protector_for_PDP.jpeg");
+                    "I echo back what you tell me. " +
+                    "Try typing `/echo hello` to see.");
                 return;
             }
+
+
+            slashCommand.replyPublic(message, "1", function() {
+                slashCommand.replyPublicDelayed(message, "2").then(slashCommand.replyPublicDelayed(message, "3"));
+            });
+
+            break;
+        default:
+            slashCommand.replyPublic(message, "I'm afraid I don't know how to " + message.command + " yet.");
+
     }
 
 })
 ;
+
